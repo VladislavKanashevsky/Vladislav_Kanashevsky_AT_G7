c02dfe4723658ebc758dc59f4e028a69bafbc1db
Vladislav_Kanashevsky <kanash1998@gmail.com>
2023-02-20 21:27:15 +0300

    delete package main and java

---
+++ src/homework/day4/ArrayAndConsole.java
@@ -0,0 +1,19 @@
+package homework.day4;
+
+public class ArrayAndConsole {
+
+    public void processArrayAndConsole() {
+        int[] array = {1, 4, 5, 6, 7, 7, 8};
+        int sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            sum += array[i];
+        }
+        System.out.println(sum);
+    }
+
+    public void processArrayAndConsoleSecond(){
+
+    }
+}
+
+
---
+++ src/homework/day4/ArrayRunner.java
@@ -0,0 +1,7 @@
+package homework.day4;
+
+public class ArrayRunner {
+    public static void main(String[] args) {
+        new ArrayAndConsole().processArrayAndConsole();
+    }
+}
