102f86887e4c71766d93f4a82e61c2a516643de8
Vladislav_Kanashevsky <kanash1998@gmail.com>
2023-02-23 00:09:16 +0300

    add new two methods

--- src/homework/day4/ArrayRunner.java
+++ src/homework/day4/ArrayRunner.java
@@ -3,11 +3,17 @@ package day4;
 public class ArrayRunner {
 
     public static void main(String[] args) {
-        int[]array={1,2,3,4,5,6,7,8,9};
-       ArrayAndConsole arrayAndConsole = new ArrayAndConsole();
-       arrayAndConsole.processArrayAndConsole(4,array);
-       arrayAndConsole.processArrayAndConsoleSecond(2,array);
-       arrayAndConsole.processArrayAndConsoleThird();
-       arrayAndConsole.processArrayAndConsoleFourth();
+
+        int[] array2 = {10, 15, 1, 4, 0, -7, 11, 8, 99};
+        ArrayAndConsole arrayAndConsole = new ArrayAndConsole();
+        System.out.println();
+        arrayAndConsole.processArrayAndConsole(2, array2);
+        System.out.println();
+        arrayAndConsole.processArrayAndConsoleSecond(5, array2);
+        System.out.println();
+        arrayAndConsole.processArrayAndConsoleThird(array2);
+        System.out.println();
+        arrayAndConsole.processArrayAndConsoleFourth();
     }
 }
+
