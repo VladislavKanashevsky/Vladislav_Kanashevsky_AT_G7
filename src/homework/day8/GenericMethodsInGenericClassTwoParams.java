ccf33b7b500ed2347742b8895af4e02e22859345
Vladislav_Kanashevsky <kanash1998@gmail.com>
2023-03-16 14:38:58 +0300

    day 8 add homework task 2 correct

--- src/homework/day8/GenericMethodsInGenericClassTwoParams.java
+++ src/homework/day8/GenericMethodsInGenericClassTwoParams.java
@@ -14,4 +14,13 @@ public class GenericMethodsInGenericClassTwoParams<X, Y> {
     public void genericMethodHalfGenArgs(X typeFour, String string) {
         System.out.println("I got an object of " + typeFour.getClass().getSimpleName() + " class and string with " + string.length() + " characters");
     }
+
+    public void genericMethodHalfGenArgs(X typeFour, Y typeSix, String stringTwo) {
+        System.out.println("I got an object of " + typeFour.getClass().getSimpleName() + " class and " + typeSix + " class and string with " + stringTwo.length() + " characters");
+    }
+
+    public void genericMethodHalfGenArgs(String stringThree, Y typeSeven) {
+        System.out.println("I got an object of " + typeSeven.getClass().getSimpleName() + " class and string with " + stringThree.length() + " characters");
+    }
+
 }
