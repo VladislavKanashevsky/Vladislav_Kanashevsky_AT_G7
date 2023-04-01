cee37177ad3144dd26b673547dc702de670c6192
Vladislav_Kanashevsky <kanash1998@gmail.com>
2023-04-01 14:28:54 +0300

    add homework day 12 task 7

---
+++ src/homework/day12/ElementsRunner.java
@@ -0,0 +1,19 @@
+package day12;
+
+import java.util.stream.Stream;
+
+public class ElementsRunner {
+
+    public static void main(String[] args) {
+
+        Stream<String> elements = Stream.of("Text field", "Radio", "Check-box", "Drop-down", "Picker", "Breadcrumb");
+
+        elements.map(element -> {
+            if (element.length() % 2 == 0) {
+                return String.valueOf(element.length());
+            } else {
+                return element.replaceAll("e", "o");
+            }
+        }).distinct().forEach(System.out::println);
+    }
+}
