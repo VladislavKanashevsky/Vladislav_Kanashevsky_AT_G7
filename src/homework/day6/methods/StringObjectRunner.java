0a2056cab74ebee67f4596bbfbe5e1b1e4717187
Vladislav_Kanashevsky <kanash1998@gmail.com>
2023-04-05 22:21:43 +0300

    add homework day 6 task 1.5

--- src/homework/day6/methods/StringObjectRunner.java
+++ src/homework/day6/methods/StringObjectRunner.java
@@ -12,7 +12,15 @@ public class StringObjectRunner {
         System.out.println();
         new MethodThird().MethodThirdProcess("String121String14546Hello6367364Stri7ng346436Hel7lo7436346String346346");
         System.out.println();
-        new MethodFourth();
+        new MethodFourth().MethodFourthProcess("access_log.2020.09.07 212.168.101.5 granted\n" +
+                "access_log.2020.09.07 212.168.101.5 granted\n" +
+                "access_log.2020.09.07 212.168.101.5 denied\n" +
+                "access_log.2020.09.07 212.168.101.5 granted\n" +
+                "access_log.2020.09.07 212.168.101.6 denied\n" +
+                "access_log.2020.09.07 212.168.101.6 denied\n" +
+                "access_log.2020.09.07 212.168.122.6 denied\n" +
+                "access_log.2020.09.07 212.168.122.6 granted\n" +
+                "access_log.2020.09.07 212.168.122.6 denied");
         System.out.println();
         new MethodFifth().MethodFifthProcess();
         System.out.println();
